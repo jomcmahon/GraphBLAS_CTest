2842a7a962148bf2fdd346fec412190279522ddd
Janice McMahon <mcmahon@reservoir.com>
2020-12-08 11:45:22 -0500

    added functionality to read test

--- testread.c
+++ testread.c
@@ -10,33 +10,44 @@
 #include "GraphBLAS.h"
 #include "util/test_utils.h"
 
-char *testbase = "testread";
-
 int main(int argc, char * argv[])
 {
-  GrB_Info info = GrB_SUCCESS;
+  GrB_Info info;
   OK(GrB_init(GrB_BLOCKING));
+  testargs *myargs = get_test_args(argc, argv);
   bool testerror = false;
 
-  printf("Running %s:\n", testbase);
+  printf("Running %s:\n", myargs->testbase); fflush(stdout);
 
+  if (strlen(myargs->input0) == 0) strcpy(myargs->input0, "A");
+  if (strlen(myargs->input1) == 0) strcpy(myargs->input1, "V1");
+  if (strlen(myargs->input2) == 0) strcpy(myargs->input2, "V2");
+  if (strlen(myargs->output) == 0) strcpy(myargs->output, "C");
+
+  char ostr[256]; strcpy(ostr, myargs->output);
   for (int i = 0; i < num_Types(); i++) {
     info = GrB_SUCCESS;
     GrB_Type thetype; get_Type(i, &thetype);
     GrB_Matrix A = NULL;
     GrB_Vector V1 = NULL, V2 = NULL;
 
-    TEST_OK(read_matlab_matrix(testbase, "A", thetype, &A));
-    TEST_OK(read_matlab_vector(testbase, "V1", thetype, &V1));
-    TEST_OK(read_matlab_vector(testbase, "V2", thetype, &V2));
+    TEST_OK(read_matlab_matrix(myargs->testbase, myargs->input0, thetype, &A));
+    TEST_OK(read_matlab_vector(myargs->testbase, myargs->input1, thetype, &V1));
+    TEST_OK(read_matlab_vector(myargs->testbase, myargs->input2, thetype, &V2));
 
-    TEST_OK(write_typed_matrix(testbase, "C", thetype, A));
-    TEST_OK(write_typed_vector(testbase, "C1", thetype, V1));
-    TEST_OK(write_typed_vector(testbase, "C2", thetype, V2));
+    TEST_OK(write_typed_matrix(myargs->testbase, myargs->output, thetype, A));
+    TEST_COND(check_typed_matrix(myargs->testbase, myargs->output, thetype, A),
+	      myargs->output);
 
-    TEST_COND(check_typed_matrix(testbase, "C", thetype, A), "testread");
-    TEST_COND(check_typed_vector(testbase, "C1", thetype, V1), "testread");
-    TEST_COND(check_typed_vector(testbase, "C2", thetype, V2), "testread");
+    sprintf(myargs->output, "%s%d", ostr, 1);
+    TEST_OK(write_typed_vector(myargs->testbase, myargs->output, thetype, V1));
+    TEST_COND(check_typed_vector(myargs->testbase, myargs->output, thetype, V1),
+	      myargs->output);
+
+    sprintf(myargs->output, "%s%d", ostr, 2);
+    TEST_OK(write_typed_vector(myargs->testbase, myargs->output, thetype, V2));
+    TEST_COND(check_typed_vector(myargs->testbase, myargs->output, thetype, V2),
+	      myargs->output);
 
     GrB_free (&A); GrB_free (&V1); GrB_free (&V2);
   }
