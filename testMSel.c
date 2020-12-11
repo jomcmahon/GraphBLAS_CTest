43b8b9aaf08ec90c12da8cf294f75bbd0bcc1933
Janice McMahon <mcmahon@reservoir.com>
2020-12-11 18:30:11 -0500

    added thunk

--- testMSel.c
+++ testMSel.c
@@ -16,6 +16,7 @@ bool run_MSel(testargs *myargs)
   GrB_Type thetype; get_Type(myargs->specobj[TYPE], &thetype);
   GrB_BinaryOp accum; get_BinaryOp(myargs->specobj[ACCUM], &accum);
   GrB_Descriptor desc; get_Descriptor(myargs->specobj[DESC], &desc);
+  GxB_Scalar scal = NULL; // thunk
 
   if (myargs->generate)  { // if generating, show accum, desc and semi
     print_args(myargs, desc, accum);
@@ -24,8 +25,13 @@ bool run_MSel(testargs *myargs)
   }
 
   GrB_Matrix A = NULL, C = NULL, M = NULL; // inputs and outputs
+  GrB_Vector T;
   GrB_Index nrA = 0, ncA = 0;
   read_matlab_matrix(myargs->inbase, myargs->input0, thetype, &A);
+  if (strlen(myargs->input1) > 0) {
+    read_matlab_vector(myargs->inbase, myargs->input1, thetype, &T); // thunk
+    OK (GxB_Scalar_new(&scal, thetype));
+  }
   get_inp_size(desc, A, &nrA, &ncA, GrB_INP0); // input 0, no desc
   if (strlen(myargs->mask) > 0) // read mask if file name given
     read_matlab_matrix(myargs->inbase, myargs->mask, GrB_BOOL, &M);
@@ -34,7 +40,26 @@ bool run_MSel(testargs *myargs)
   if (strlen(myargs->initvals) == 0) OK (GrB_Matrix_new(&C, thetype, nrA, ncA));
   else read_matlab_matrix(myargs->inbase, myargs->initvals, thetype, &C);
 
-  OK (GxB_select(C, M, accum, selop, A, NULL, desc)); // do the operation
+#define SET_AND_TEST				\
+  OK (GrB_Vector_extractElement(&c, T, 0));	\
+  OK (GxB_Scalar_setElement(scal, c));
+
+  if (T) { // set thunk based on type
+    if (thetype == GrB_BOOL) { bool c; SET_AND_TEST; }
+    else if (thetype == GrB_INT8) { int8_t c; SET_AND_TEST; }
+    else if (thetype == GrB_INT16) { int16_t c; SET_AND_TEST; }
+    else if (thetype == GrB_INT32) { int32_t c; SET_AND_TEST; }
+    else if (thetype == GrB_INT64) { int64_t c; SET_AND_TEST; }
+    else if (thetype == GrB_UINT8) { uint8_t c; SET_AND_TEST; }
+    else if (thetype == GrB_UINT16) { uint16_t c; SET_AND_TEST; }
+    else if (thetype == GrB_UINT32) { uint32_t c; SET_AND_TEST; }
+    else if (thetype == GrB_UINT64) { uint64_t c; SET_AND_TEST; }
+    else if (thetype == GrB_FP32) { float c; SET_AND_TEST; }
+    else if (thetype == GrB_FP64) { double c; SET_AND_TEST; }
+    else { printf("bad type\n"); exit(1); }
+  }
+
+  OK (GxB_select(C, M, accum, selop, A, scal, desc)); // do the operation
 
   bool testerror = false; // if generating, write to file, otherwise compare
   if (myargs->generate) write_typed_matrix(myargs->testbase, myargs->output, C);
--- testVSel.c
+++ testVSel.c
@@ -16,6 +16,7 @@ bool run_VSel(testargs *myargs)
   GrB_Type thetype; get_Type(myargs->specobj[TYPE], &thetype);
   GrB_BinaryOp accum; get_BinaryOp(myargs->specobj[ACCUM], &accum);
   GrB_Descriptor desc; get_Descriptor(myargs->specobj[DESC], &desc);
+  GxB_Scalar scal = NULL; // thunk
 
   if (myargs->generate)  { // if generating, show accum, desc and semi
     print_args(myargs, desc, accum);
@@ -23,9 +24,13 @@ bool run_VSel(testargs *myargs)
     OK (GxB_print(selop, GxB_SUMMARY));
   }
 
-  GrB_Vector A = NULL, C = NULL, M = NULL; // inputs and outputs
+  GrB_Vector T = NULL, A = NULL, C = NULL, M = NULL; // inputs and outputs
   GrB_Index vsize = 0;
   read_matlab_vector(myargs->inbase, myargs->input0, thetype, &A);
+  if (strlen(myargs->input1) > 0) {
+    read_matlab_vector(myargs->inbase, myargs->input1, thetype, &T); // thunk
+    OK (GxB_Scalar_new(&scal, thetype));
+  }
   OK (GrB_Vector_size(&vsize, A));
   if (strlen(myargs->mask) > 0) // read mask if file name given
     read_matlab_vector(myargs->inbase, myargs->mask, GrB_BOOL, &M);
@@ -34,7 +39,26 @@ bool run_VSel(testargs *myargs)
   if (strlen(myargs->initvals) == 0) OK (GrB_Vector_new(&C, thetype, vsize));
   else read_matlab_vector(myargs->inbase, myargs->initvals, thetype, &C);
 
-  OK (GxB_select(C, M, accum, selop, A, NULL, desc)); // do the operation
+#define SET_AND_TEST				\
+  OK (GrB_Vector_extractElement(&c, T, 0));	\
+  OK (GxB_Scalar_setElement(scal, c));
+
+  if (T) { // set thunk based on type
+    if (thetype == GrB_BOOL) { bool c; SET_AND_TEST; }
+    else if (thetype == GrB_INT8) { int8_t c; SET_AND_TEST; }
+    else if (thetype == GrB_INT16) { int16_t c; SET_AND_TEST; }
+    else if (thetype == GrB_INT32) { int32_t c; SET_AND_TEST; }
+    else if (thetype == GrB_INT64) { int64_t c; SET_AND_TEST; }
+    else if (thetype == GrB_UINT8) { uint8_t c; SET_AND_TEST; }
+    else if (thetype == GrB_UINT16) { uint16_t c; SET_AND_TEST; }
+    else if (thetype == GrB_UINT32) { uint32_t c; SET_AND_TEST; }
+    else if (thetype == GrB_UINT64) { uint64_t c; SET_AND_TEST; }
+    else if (thetype == GrB_FP32) { float c; SET_AND_TEST; }
+    else if (thetype == GrB_FP64) { double c; SET_AND_TEST; }
+    else { printf("bad type\n"); exit(1); }
+  }
+
+  OK (GxB_select(C, M, accum, selop, A, scal, desc)); // do the operation
 
   bool testerror = false; // if generating, write to file, otherwise compare
   if (myargs->generate) write_typed_vector(myargs->testbase, myargs->output, C);
