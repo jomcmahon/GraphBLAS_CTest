f318e7ff14dd017e0da053946dffa568ca0e3c35
Janice McMahon <mcmahon@reservoir.com>
2020-12-10 22:59:14 -0500

    new write routines

--- util/mm_routines.c
+++ util/mm_routines.c
@@ -9,14 +9,19 @@
 
 #include "GraphBLAS.h"
 #include "test_utils.h"
-#include "mmio.h"
 
 void mm_setup(FILE *, int *, int *, int *, GrB_Index **, GrB_Index **,
 	      double **);
 void mm_double(FILE *, GrB_Type, int, GrB_Index *, GrB_Index *, double *);
 void mm_type(FILE *, GrB_Type, int, GrB_Index *, GrB_Index *, double *);
-void mm_matbuild(GrB_Matrix, GrB_Type, int, GrB_Index *, GrB_Index *, double *);
 void mm_vecbuild(GrB_Vector, GrB_Type, int, GrB_Index *, double *);
+void mm_matbuild(GrB_Matrix, GrB_Type, int, GrB_Index *, GrB_Index *, double *);
+
+void mm_output(FILE *, int, int, int, GrB_Index **, GrB_Index **, double **);
+void mm_print(FILE *, GrB_Type, int, GrB_Index *, GrB_Index *, double *);
+void mm_vecextract(GrB_Vector, GrB_Type, GrB_Index, GrB_Index *, double *);
+void mm_matextract(GrB_Matrix, GrB_Type, GrB_Index, GrB_Index *, GrB_Index *,
+		   double *);
 
 // read a matrix written by matlab into a GraphBLAS matrix of a certain type
 void mm_matlab(GrB_Matrix *A, FILE *f, GrB_Type thetype)
@@ -90,4 +95,48 @@ void vec_read(GrB_Vector *A, FILE *f, GrB_Type thetype)
   mm_type(f, thetype, nz, I, J, X_f64);
   if (M == 1) mm_vecbuild(*A, thetype, nz, J, X_f64); // row vector
   else mm_vecbuild(*A, thetype, nz, I, X_f64); // column vector
+}
+
+// write a GraphBLAS matrix to matrix market file
+void mm_write(GrB_Matrix A, FILE *f)
+{
+  if ((f == NULL) || (A == NULL))
+    { printf("file or object pointer is null\n"); exit(1); }
+
+  GrB_Info info;
+  GrB_Type thetype;
+  GrB_Index M, N, nz;
+  GrB_Index *I, *J;
+  double *X_f64;
+
+  OK (GxB_Matrix_type (&thetype, A));
+  OK (GrB_Matrix_nrows(&M, A));
+  OK (GrB_Matrix_ncols(&N, A));
+  OK (GrB_Matrix_nvals(&nz, A));
+
+  mm_output(f, M, N, nz, &I, &J, &X_f64);
+  mm_matextract(A, thetype, nz, I, J, X_f64);
+  mm_print(f, thetype, nz, I, J, X_f64);
+}
+
+// write GraphBLAS vector to matrix market file
+void vec_write(GrB_Vector A, FILE *f)
+{
+  if ((f == NULL) || (A == NULL))
+    { printf("file or object pointer is null\n"); exit(1); }
+
+  GrB_Info info;
+  GrB_Type thetype;
+  GrB_Index M = 1, N, nz;
+  GrB_Index *I, *J;
+  double *X_f64;
+
+  OK (GxB_Vector_type (&thetype, A));
+  OK (GrB_Vector_size(&N, A));
+  OK (GrB_Vector_nvals(&nz, A));
+
+  mm_output(f, M, N, nz, &I, &J, &X_f64);
+  mm_vecextract(A, thetype, nz, J, X_f64);
+  for (int i = 0; i < nz; i++) I[i] = 0LU; // row vector
+  mm_print(f, thetype, nz, I, J, X_f64);
 }
--- util/mm_support.c
+++ util/mm_support.c
@@ -172,3 +172,118 @@ void mm_vecbuild(GrB_Vector A, GrB_Type thetype, int nz, GrB_Index *I,
     GrB_Vector_build(A, I, (double *)X_f64, nz, GxB_ANY_FP64);
   else { printf("bad type\n"); exit(1); }
 }
+
+// read banner and allocate arrays
+void mm_output(FILE *f, int M, int N, int nz, GrB_Index **I, GrB_Index **J,
+	       double **X_f64)
+{
+  MM_typecode matcode;
+
+  mm_set_matrix(&matcode);
+  mm_set_coordinate(&matcode);
+  mm_set_real(&matcode);
+  mm_set_general(&matcode);
+
+  if (mm_write_banner(f, matcode) != 0)
+    { printf("couldn't write mtx banner\n"); exit(1); }
+
+  if (mm_write_mtx_crd_size(f, M, N, nz) !=0)
+    { printf("couldn't write sizes\n"); exit(1); }
+
+  *I = malloc(nz * sizeof(GrB_Index));
+  *J = malloc(nz * sizeof(GrB_Index));
+  *X_f64 = malloc(nz * sizeof(double));
+}
+
+// read values as given type
+void mm_print(FILE *f, GrB_Type thetype, int nz, GrB_Index *I, GrB_Index *J,
+	      double *X_f64)
+{
+  for (int k = 0; k < nz; k++) {
+    if (thetype == GrB_BOOL) { bool *X = (bool *)X_f64;
+      fprintf(f, "%lu %lu %d\n", I[k]+1, J[k]+1, X[k]);
+    } else if (thetype == GrB_INT8) { int8_t *X = (int8_t *)X_f64;
+      fprintf(f, "%lu %lu %hhd\n", I[k]+1, J[k]+1, X[k]);
+    } else if (thetype == GrB_INT16) { int16_t *X = (int16_t *)X_f64;
+      fprintf(f, "%lu %lu %hd\n", I[k]+1, J[k]+1, X[k]);
+    } else if (thetype == GrB_INT32) { int32_t *X = (int32_t *)X_f64;
+      fprintf(f, "%lu %lu %d\n", I[k]+1, J[k]+1, X[k]);
+    } else if (thetype == GrB_INT64) { int64_t *X = (int64_t *)X_f64;
+      fprintf(f, "%lu %lu %ld\n", I[k]+1, J[k]+1, X[k]);
+    } else if (thetype == GrB_UINT8) { uint8_t *X = (uint8_t *)X_f64;
+      fprintf(f, "%lu %lu %hhu\n", I[k]+1, J[k]+1, X[k]);
+    } else if (thetype == GrB_UINT16) { uint16_t *X = (uint16_t *)X_f64;
+      fprintf(f, "%lu %lu %hu\n", I[k]+1, J[k]+1, X[k]);
+    } else if (thetype == GrB_UINT32) { uint32_t *X = (uint32_t *)X_f64;
+      fprintf(f, "%lu %lu %u\n", I[k]+1, J[k]+1, X[k]);
+    } else if (thetype == GrB_UINT64) { uint64_t *X = (uint64_t *)X_f64;
+      fprintf(f, "%lu %lu %lu\n", I[k]+1, J[k]+1, X[k]);
+    } else if (thetype == GrB_FP32) { float *X = (float *)X_f64;
+      fprintf(f, "%lu %lu %.16f\n", I[k]+1, J[k]+1, X[k]);
+    } else if (thetype == GrB_FP64) { double *X = (double *)X_f64;
+      fprintf(f, "%lu %lu %.16f\n", I[k]+1, J[k]+1, X[k]);
+    } else { printf("bad type\n"); exit(1); }
+  }
+}
+
+// build matrix from arrays according to type
+void mm_matextract(GrB_Matrix A, GrB_Type thetype, GrB_Index nv, GrB_Index *I,
+		   GrB_Index *J, double *X_f64)
+{
+  GrB_Index nz;
+  if (thetype == GrB_BOOL)
+    GrB_Matrix_extractTuples(I, J, (bool *)X_f64, &nz, A);
+  else if (thetype == GrB_INT8)
+    GrB_Matrix_extractTuples(I, J, (int8_t *)X_f64, &nz, A);
+  else if (thetype == GrB_INT16)
+    GrB_Matrix_extractTuples(I, J, (int16_t *)X_f64, &nz, A);
+  else if (thetype == GrB_INT32)
+    GrB_Matrix_extractTuples(I, J, (int32_t *)X_f64, &nz, A);
+  else if (thetype == GrB_INT64)
+    GrB_Matrix_extractTuples(I, J, (int64_t *)X_f64, &nz, A);
+  else if (thetype == GrB_UINT8)
+    GrB_Matrix_extractTuples(I, J, (uint8_t *)X_f64, &nz, A);
+  else if (thetype == GrB_UINT16)
+    GrB_Matrix_extractTuples(I, J, (uint16_t *)X_f64, &nz, A);
+  else if (thetype == GrB_UINT32)
+    GrB_Matrix_extractTuples(I, J, (uint32_t *)X_f64, &nz, A);
+  else if (thetype == GrB_UINT64)
+    GrB_Matrix_extractTuples(I, J, (uint64_t *)X_f64, &nz, A);
+  else if (thetype == GrB_FP32)
+    GrB_Matrix_extractTuples(I, J, (float *)X_f64, &nz, A);
+  else if (thetype == GrB_FP64)
+    GrB_Matrix_extractTuples(I, J, (double *)X_f64, &nz, A);
+  else { printf("bad type\n"); exit(1); }
+  if (nz != nv) { printf("Bad number of vals %lu %lu\n", nz, nv); exit(1); }
+}
+
+// build matrix from arrays according to type
+void mm_vecextract(GrB_Vector A, GrB_Type thetype, GrB_Index nv, GrB_Index *I,
+		   double *X_f64)
+{
+  GrB_Index nz;
+  if (thetype == GrB_BOOL)
+    GrB_Vector_extractTuples(I, (bool *)X_f64, &nz, A);
+  else if (thetype == GrB_INT8)
+    GrB_Vector_extractTuples(I, (int8_t *)X_f64, &nz, A);
+  else if (thetype == GrB_INT16)
+    GrB_Vector_extractTuples(I, (int16_t *)X_f64, &nz, A);
+  else if (thetype == GrB_INT32)
+    GrB_Vector_extractTuples(I, (int32_t *)X_f64, &nz, A);
+  else if (thetype == GrB_INT64)
+    GrB_Vector_extractTuples(I, (int64_t *)X_f64, &nz, A);
+  else if (thetype == GrB_UINT8)
+    GrB_Vector_extractTuples(I, (uint8_t *)X_f64, &nz, A);
+  else if (thetype == GrB_UINT16)
+    GrB_Vector_extractTuples(I, (uint16_t *)X_f64, &nz, A);
+  else if (thetype == GrB_UINT32)
+    GrB_Vector_extractTuples(I, (uint32_t *)X_f64, &nz, A);
+  else if (thetype == GrB_UINT64)
+    GrB_Vector_extractTuples(I, (uint64_t *)X_f64, &nz, A);
+  else if (thetype == GrB_FP32)
+    GrB_Vector_extractTuples(I, (float *)X_f64, &nz, A);
+  else if (thetype == GrB_FP64)
+    GrB_Vector_extractTuples(I, (double *)X_f64, &nz, A);
+  else { printf("bad type\n"); exit(1); }
+  if (nz != nv) { printf("Bad number of vals %lu %lu\n", nz, nv); exit(1); }
+}
